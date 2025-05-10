1b8ff15662fa6f7163effcd04dc9044b7ad88d94
Kawsar <kawsarmahmud822@gmail.com>
2025-05-10 23:32:41 +0600

    First Commig

---
+++ Day - 1/script.js
@@ -0,0 +1,137 @@
+age = 10
+console.log("Hello World")
+console.log(10)
+
+
+let channelName = "Aila Jadu";
+let videos = 455;
+let suscribers = 1000000;
+
+console.log("Channel Name: " + channelName);
+console.log("Videos: " + videos);
+console.log("Suscribers: " + suscribers);
+
+
+product_name: "Stylish Sunglasses";
+price: 99;
+originalPrice: 299;
+discount: "67%";
+
+console.log("Product Name: " + product_name);
+console.log("Price: " + price);
+console.log("Original Price: " + originalPrice);
+console.log("Discount: " + discount);
+
+// Comments
+// This is a single line comment
+/*
+This is a multi-line comment
+This is a multi-line comment
+*/
+
+
+// Operants
+// Arithmetic Operators
+// +, -, *, /, %, ++, --
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log("Addition: " + c);
+
+let d = a - b;
+console.log("Subtraction: " + d);
+
+let e = a * b;
+console.log("Multiplication: " + e);
+
+let f = a / b;
+console.log("Division: " + f);
+
+let g = a % b;
+console.log("Modulus: " + g);
+
+
+// Increment and Decrement
+let h = 10;
+h++;
+console.log("Increment: " + h);
+h--;
+console.log("Decrement: " + h);
+
+// Assignment Operators
+// =, +=, -=, *=, /=, %=, **=
+let i = 10;
+i += 5;
+console.log("Addition Assignment: " + i);
+
+i -= 5;
+console.log("Subtraction Assignment: " + i);
+
+i *= 5;
+console.log("Multiplication Assignment: " + i);
+
+i /= 5;
+console.log("Division Assignment: " + i);
+
+i %= 5;
+console.log("Modulus Assignment: " + i);
+
+x = 10;
+p = 2;
+x **= p;
+console.log("Exponentiation Assignment: " + x);
+
+// Comparison Operators
+// ==, ===, !=, !==, >, <, >=, <=
+
+let j = 10;
+let k = 20;
+console.log("Equal: " + (j == k));
+console.log("Strict Equal: " + (j === k));
+console.log("Not Equal: " + (j != k));
+console.log("Strict Not Equal: " + (j !== k));
+console.log("Greater Than: " + (j > k));
+console.log("Less Than: " + (j < k));
+console.log("Greater Than or Equal: " + (j >= k));
+console.log("Less Than or Equal: " + (j <= k));
+
+// Logical Operators
+// &&, ||, !
+let l = true;
+let m = false;
+console.log("Logical AND: " + (l && m));
+console.log("Logical OR: " + (l || m));
+console.log("Logical NOT: " + (!l));
+
+// Bitwise Operators
+// &, |, ^, ~, <<, >>
+let n = 10; // 1010
+let o = 4; // 0100
+console.log("Bitwise AND: " + (n & o)); // 0000
+console.log("Bitwise OR: " + (n | o)); // 1110
+console.log("Bitwise XOR: " + (n ^ o)); // 1110
+console.log("Bitwise NOT: " + (~n)); // 0101
+console.log("Left Shift: " + (n << 1)); // 10100
+console.log("Right Shift: " + (n >> 1)); // 0101
+
+// Ternary Operator
+// condition ? expr1 : expr2
+let p = 10;
+let q = 20;
+let result = (p > q) ? "p is greater" : "q is greater";
+console.log(result);
+
+// Conditional Statements
+// if, else if, else
+
+let r = 10;
+let s = 20;
+if (r > s) {
+    console.log("r is greater");
+}
+else if (r < s) {
+    console.log("s is greater");
+} else {
+    console.log("Both are equal");
+}
+
